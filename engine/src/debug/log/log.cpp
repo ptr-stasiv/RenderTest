88d4aabea027ad2536241c089ffbdbe46af3d367
Petro Stasiv <ptrstasiv@gmail.com>
2021-01-31 22:00:22 +0200

    Fast console log sync fix

--- engine/src/debug/log/log.cpp
+++ engine/src/debug/log/log.cpp
@@ -4,12 +4,16 @@
 #include <cstdarg>
 #include <Windows.h>
 
+#include "utils/sync/spin-lock.h"
+
 static const char* g_FontColorModes[] =
 {
    "\033[0m",  //Default
    "\033[31m", //Red
    "\033[33m"  //Yellow
 };
+
+static utils::sync::SpinLock g_CmdSpinLock;
 
 void PrintLog(const LogLevels level, const char* filepath, const int line, const const char* format, ...)
 {
@@ -30,6 +34,8 @@ void PrintLog(const LogLevels level, const char* filepath, const int line, const
       break;
    }
 
+   g_CmdSpinLock.lock();
+
    printf("%s- %d:%d:%d %s %s:%d ", g_FontColorModes[level], st.wHour, st.wMinute, st.wSecond, message, filepath, line);
   
    va_list args;
@@ -38,10 +44,14 @@ void PrintLog(const LogLevels level, const char* filepath, const int line, const
    va_end(args);
 
    printf("%s\n\n", g_FontColorModes[0]);
+
+   g_CmdSpinLock.unlock();
 }
 
 void PrintLogWD(const LogLevels level, const const char* format, ...)
 {
+   g_CmdSpinLock.lock();
+
    printf("%s- ", g_FontColorModes[level]);
 
    va_list args;
@@ -50,4 +60,6 @@ void PrintLogWD(const LogLevels level, const const char* format, ...)
    va_end(args);
 
    printf("%s\n\n", g_FontColorModes[0]);
+
+   g_CmdSpinLock.unlock();
 }
