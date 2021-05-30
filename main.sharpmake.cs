dfa6d725e2257b34550939ca27c76d487126e26b
Petro <ptrstasiv@gmail.com>
2021-05-30 09:19:50 +0300

    Remove gui project

--- main.sharpmake.cs
+++ main.sharpmake.cs
@@ -3,61 +3,6 @@
 namespace sharpmake
 {
     [Generate]
-    public class EngineGuiProject : Project
-    {
-        public EngineGuiProject()
-        {
-            Name = "EngineGUI";
-
-            SourceRootPath = @"[project.SharpmakeCsPath]/gui/src";
-
-            AddTargets(new Target(Platform.win64, DevEnv.vs2019, Optimization.Debug | Optimization.Release | Optimization.Retail));
-        }
-
-        [Configure]
-        public void ConfigureAll(Project.Configuration config, Target target)
-        {
-            config.Options.Add(Options.Vc.General.WindowsTargetPlatformVersion.Latest);
-            config.Options.Add(Options.Vc.General.WarningLevel.EnableAllWarnings);
-
-            config.Options.Add(Options.Vc.Compiler.CppLanguageStandard.CPP17);
-
-            config.ProjectPath = @"[project.SharpmakeCsPath]/gui";
-
-
-            //Defines
-
-            config.Defines.Add("GLEW_STATIC");
-
-            if (target.Optimization == Optimization.Debug)
-                config.Defines.Add("DEBUG");
-            else
-                config.Defines.Add("RELEASE");
-
-
-            if (target.Platform == Platform.win64)
-                config.Defines.Add("WINDOWS");
-
-
-            config.IncludePaths.Add(@"[project.SharpmakeCsPath]/gui/extern/ul/include");
-            config.LibraryPaths.Add(@"[project.SharpmakeCsPath]/gui/extern/ul/lib");
-
-            config.IncludePaths.Add(@"[project.SharpmakeCsPath]/engine/src");
-            config.IncludePaths.Add(@"[project.SharpmakeCsPath]/gui/src");
-
-            config.LibraryFiles.AddRange(new Strings("AppCore", "Ultralight", "UltralightCore", "WebCore", "wsock32"));
-
-            config.Output = Configuration.OutputType.Lib;
-
-            config.TargetPath = @"[project.SharpmakeCsPath]/gui/binaries/[target.Optimization]";
-            config.IntermediatePath = @"[project.SharpmakeCsPath]/gui/binaries/int/[target.Optimization]";
-
-            if (target.Optimization == Optimization.Debug)
-                config.Options.Add(Options.Vc.Compiler.RuntimeLibrary.MultiThreaded);
-        }
-    }
-
-    [Generate]
     public class RenderTestProject : Project
     {
         public RenderTestProject()
@@ -108,10 +53,7 @@ namespace sharpmake
             config.LibraryPaths.Add(@"[project.SharpmakeCsPath]/engine/extern/glew/lib");
 
 
-            config.LibraryFiles.AddRange(new Strings("opengl32", "glfw3", "glew32s", "msvcrt"));
-
-
-            config.AddPrivateDependency<EngineGuiProject>(target);
+            config.LibraryFiles.AddRange(new Strings("opengl32", "glfw3", "glew32s", "msvcrt"));;
 
 
             config.Output = Configuration.OutputType.Exe;
@@ -180,7 +122,6 @@ namespace sharpmake
         {
             config.SolutionPath = @"[solution.SharpmakeCsPath]";
 
-            config.AddProject<EngineGuiProject>(target);
             config.AddProject<RenderTestProject>(target);
             config.AddProject<TestsProject>(target);
 
